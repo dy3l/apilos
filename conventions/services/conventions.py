bb00b5b8d5717db56daf735cdd1a0193f56f544b
Marjolaine <marjolaine.le-bon@beta.gouv.fr>
2023-02-16 15:35:26 +0100

    init new way to check form

--- conventions/services/conventions.py
+++ conventions/services/conventions.py
@@ -153,25 +153,28 @@ def convention_post_action(request, convention_uuid):
     convention = Convention.objects.get(uuid=convention_uuid)
     result_status = None
     form_posted = None
+    is_resiliation = request.POST.get("resiliation", False)
     if request.method == "POST":
-        resiliation_form = ConventionResiliationForm(request.POST)
-        if resiliation_form.is_valid():
-            convention.statut = ConventionStatut.RESILIEE
-            convention.date_resiliation = resiliation_form.cleaned_data[
-                "date_resiliation"
-            ]
-            convention.save()
-            # SUCCESS
-            result_status = utils.ReturnStatus.SUCCESS
-            form_posted = "resiliation"
-        updatedate_form = ConventionDateForm(request.POST)
-        if updatedate_form.is_valid():
-            convention.televersement_convention_signee_le = updatedate_form.cleaned_data[
-                "televersement_convention_signee_le"
-            ]
-            convention.save()
-            result_status = utils.ReturnStatus.SUCCESS
-            form_posted = "date_signature"
+        if is_resiliation:
+            resiliation_form = ConventionResiliationForm(request.POST)
+            if resiliation_form.is_valid():
+                convention.statut = ConventionStatut.RESILIEE
+                convention.date_resiliation = resiliation_form.cleaned_data[
+                    "date_resiliation"
+                ]
+                convention.save()
+                # SUCCESS
+                result_status = utils.ReturnStatus.SUCCESS
+                form_posted = "resiliation"
+        else:
+            updatedate_form = ConventionDateForm(request.POST)
+            if updatedate_form.is_valid():
+                convention.televersement_convention_signee_le = updatedate_form.cleaned_data[
+                    "televersement_convention_signee_le"
+                ]
+                convention.save()
+                result_status = utils.ReturnStatus.SUCCESS
+                form_posted = "date_signature"
 
     else:
         resiliation_form = ConventionResiliationForm()
